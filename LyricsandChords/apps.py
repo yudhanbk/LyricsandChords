431e2568ef95461a1335698cf0e99e48336a4609
yudhanbk <yudhanbishwakarma10@gmail.com>
2025-07-19 14:00:30 +0545

    This is the app where you can add your lyrics and chords

---
+++ LyricsandChords/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LyricsandchordsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LyricsandChords'
---
+++ LyricsandChords/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .models import Song
+from .forms import RegisterForm
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'LyricsandChords/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('song_list')
+    return render(request, 'LyricsandChords/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'LyricsandChords/song_list.html', {'songs': songs})
